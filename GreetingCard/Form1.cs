030102bc81e36c6556c451964659978e61159502
Tony Theodoropoulos <theodot@outlook.com>
2019-09-23 12:37:26 -0400

    Add project files.

---
+++ GreetingCard/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GreetingCard
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            Font f = new Font("Arial", 12);
+            SolidBrush sb = new SolidBrush(Color.Black);
+
+            g.DrawString("GREETING CARD", f, sb, 50, 50);
+        }
+    }
+}
